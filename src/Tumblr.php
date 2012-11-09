81a359c1c1d0064035646d21ff655585369e3962
Jaap Broekhuizen <jaapz.b@gmail.com>
2012-11-09 15:37:26 +0100

    Fix indentation.

--- src/Tumblr.php
+++ src/Tumblr.php
@@ -117,7 +117,7 @@ class Tumblr
 
 		// Get data.
 		$curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		$response = curl_exec($curl);
 		curl_close($curl);
 
